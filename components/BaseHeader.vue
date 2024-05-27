8df2ae3d273fa805aa0d3d212e5da8d493f865cd
Jovana_Matic <jovana.stankovic85@yahoo.com>
2024-05-27 12:53:39 +0200

    Add pages index.vue file, layputs and base header component

---
+++ components/BaseHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>
+      <nav>
+        <ul>
+          <li class="active">
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Contact</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .active {
+    background-color: #04AA6D;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+
+  li {
+    float: left;
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  li a:hover:not(.active) {
+    background-color: #111;
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <BaseHeader />
+    <main id="main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+body {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+}
+
+</style>
