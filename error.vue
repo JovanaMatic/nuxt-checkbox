2d7bf0f6d80ae7db05229f36bdad35e5eb315152
Jovana_Matic <jovana.stankovic85@yahoo.com>
2024-06-01 16:37:23 +0200

    Add error page

---
+++ error.vue
@@ -0,0 +1,25 @@
+<script setup>
+  defineProps({
+    error: {
+      type: Object,
+      required: true
+    }
+  })
+</script>
+
+<template>
+  <div class="error-page">
+    <h1>Ooopsy you found our error page</h1>
+    <p>{{ error.message }}</p>
+    <p>{{ error.status }}</p>
+    <p>Go back to safe zone: 
+      <NuxtLink to="/">Go to Homepage</NuxtLink>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+  .error-page {
+    padding: 10px;
+  }
+</style>
